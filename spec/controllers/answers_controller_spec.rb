c80717b686b419961bc3aac557ec216353efa567
zealot <zealot@zealot-2.local>
2017-05-25 23:49:00 +0300

    Rspec for answer-controller is working

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
 
   describe 'GET #new' do
-    before { get :new, params: {question_id: question} }
+    before { get :new, {question_id: question.id} }
 
     it 'Assigns a new Answer to @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
@@ -13,6 +13,31 @@ RSpec.describe AnswersController, type: :controller do
 
     it 'render new view' do
       expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new answer in the database' do
+        expect { post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirect to show question view' do
+        post :create, { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        post :create, { question_id: question, answer: attributes_for(:invalid_answer) }
+        expect(response).to render_template :new
+      end
+
+      it 're-renders new view' do
+        post :create, { question_id: question, answer: attributes_for(:invalid_answer) }
+        expect(response).to render_template :new
+      end
     end
   end
 end
