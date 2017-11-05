ee857358cf4bfb58027af99209a82458b44baff1
zealot <zealot@zealot-2.local>
2017-11-05 14:56:35 +0300

    Addes tests

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :attachment do
-    file File.new("#{Rails.root}/spec/spec_helper.rb")
+    # file File.new("#{Rails.root}/spec/spec_helper.rb")
     association :attachable
   end
 end
