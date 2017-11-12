0f13a7d9820e0c923f4ba61ea5a301133ff7ec8f
zealot <zealot@zealot-2.local>
2017-11-12 14:11:30 +0300

    Fix specs

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,16 +1,25 @@
+require 'rails_helper'
+
 RSpec.describe Search do
   context '.results' do
     let(:query) { 'some query' }
-    let(:params) { {classes: [Question], page: '1', per_page: 5} }
 
-    it 'calls escape request' do
-      expect(ThinkingSphinx::Query).to receive(:escape).with(query).and_call_original
-      Search.results(query, ['question'], '1')
+    %w(question answer comment user).each do |attr|
+      it 'calls escape request' do
+        expect(ThinkingSphinx::Query).to receive(:escape).with(query).and_call_original
+        Search.results(query, [attr], '1')
+      end
     end
 
-    it 'calls ThinkingSphinx.search' do
-      expect(ThinkingSphinx).to receive(:search).with(query, params).and_call_original
-      Search.results(query, ['question'], '1')
+    %w(question answer comment user).each do |attr|
+      it 'calls ThinkingSphinx.search' do
+        expect(ThinkingSphinx).to receive(:search).with(query, classes: [attr.singularize.classify.constantize], page: '1', per_page: 5).and_call_original
+        Search.results(query, [attr], '1')
+      end
+    end
+
+    it 'return nil with other class' do
+      expect(Search.results(query, 'Other', '1')).to be_nil
     end
   end
 end
