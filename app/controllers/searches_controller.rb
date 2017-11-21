52b44c0902ae68f1546915480993428c725471fa
zealot <zealot@zealot-2.local>
2017-11-21 23:50:44 +0300

    DRY in searches_conroller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,11 +3,8 @@ class SearchesController < ApplicationController
   authorize_resource
 
   def show
-    if params[:text] && params[:scopes]
-      @results = Search.results(params[:text], params[:scopes], params[:page])
-      respond_with(@results)
-    elsif params[:text]
-      params[:scopes] = Search::TYPES
+    params[:scopes] = Search::TYPES if params[:scopes].blank?
+    if params[:text]
       @results = Search.results(params[:text], params[:scopes], params[:page])
       respond_with(@results)
     end
