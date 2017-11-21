63c381949c1a0cb0f518c0dc66180f09db78971d
zealot <zealot@zealot-2.local>
2017-11-21 23:16:03 +0300

    Search without params

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,10 @@ class SearchesController < ApplicationController
     if params[:text] && params[:scopes]
       @results = Search.results(params[:text], params[:scopes], params[:page])
       respond_with(@results)
+    elsif params[:text]
+      params[:scopes] = Search::TYPES
+      @results = Search.results(params[:text], params[:scopes], params[:page])
+      respond_with(@results)
     end
   end
 end
